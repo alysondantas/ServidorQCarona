8ac048245965dc9fa3088385c78f610d8fd012a7
Alyson Dantas <alyson.lipe@gmail.com>
2018-02-15 18:44:58 -0300

    integrando protocolo editar perfil

--- src/br/com/qcarona/model/Protocolo.java
+++ src/br/com/qcarona/model/Protocolo.java
@@ -17,6 +17,7 @@ public abstract class Protocolo {
         public static final int BUSCAR_USUARIO_EMAIL = 2;
         public static final int SOLICITAR_AMIZADE = 3;
         public static final int CARONAS_DISPONIVEIS = 4;
+        public static final int EDITAR_PERFIL = 5;
     }
 
     public abstract class Notificacao {
